1aa27428ddbd3703c65cc0d2755faf38a4b2d466
“RakibHasanDev” <rakibkst16@gmail.com>
2023-02-27 05:24:12 -0800

    added new section

--- src/Components/Pages/Home/Banner/Banner.js
+++ src/Components/Pages/Home/Banner/Banner.js
@@ -13,20 +13,20 @@ const Banner = () => {
         style={{ backgroundImage: `url(${background})` }}
       >
         <div className="max-w-2xl tracking-[1px] mx-auto pt-40 md:pt-[30vh] ">
-          
+
           <h1 className="md:text-6xl text-2xl px-[15px] text-white font-[PlayfairDisplay] mb-[45px]">RIGHT HOME CARE
             FOR YOU</h1>
-            <h4 className="md:text-2xl text-sm font-semibold text-[#dbf2f4] font-[Roboto] mb-8 px-5 md:px-0">
+          <h4 className="md:text-2xl text-sm font-semibold text-[#dbf2f4] font-[Roboto] mb-8 px-5 md:px-0">
             “HHA/PCA & CDPAP SERVICES THROUGHOUT NEW YORK CITY”
           </h4>
         </div>
-        <div className= ' mt-[10vh] flex justify-center text-white font-semibold gap-8'>
-                    <a href="tel:+1516-367-2266" className=' px-12 py-4 rounded-full lg:text-xl bg-primary uppercase  bg-hov2'>
-                        Contact Us
-                    </a>
-                    
+        <div className=' mt-[10vh] flex justify-center text-white font-semibold gap-8'>
+          <a href="tel:+1516-367-2266" className=' px-12 py-4 rounded-full lg:text-xl bg-primary uppercase  bg-hov2'>
+            Contact Us
+          </a>
 
-                </div>
+
+        </div>
       </div>
       <div className="grid grid-cols-3 ">
         <div className="flex justify-center bg-[#DBF2F4] py-6">
@@ -46,7 +46,7 @@ const Banner = () => {
           <div className=" flex items-center lg:gap-5 gap-3  ">
             <img src={peace} alt="" className="lg:w-14 w-8 h-8 lg:h-14" />
             <p className="lg:text-xl text-sm font-semibold text-white">
-            Experienced Home Health Aides
+              Experienced Home Health Aides
             </p>
           </div>
         </div>
--- src/Components/Pages/Home/Stressed/Stressed.js
+++ src/Components/Pages/Home/Stressed/Stressed.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Stressed = () => {
     return (
         <div>
-            
+            <h1>this is stressed</h1>
         </div>
     );
 };
