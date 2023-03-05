f282598cafb8acd71d9836f2d4e8218e0a2df599
Razibul-Islam <razibulislam665@gmail.com>
2023-03-05 23:32:33 +0600

    change

--- src/Components/Pages/Contacts/Contacts/Contacts.js
+++ src/Components/Pages/Contacts/Contacts/Contacts.js
@@ -1,12 +1,120 @@
-import React from 'react';
+import React from "react";
 
 const Contacts = () => {
-    return (
-        <div>
-            <h1 className='
-            text-center'> Contacts </h1>
+  const now = new Date().toLocaleDateString("en-US", { weekday: "long" });
+  console.log(now);
+
+  let val;
+  let day;
+  let text;
+
+  switch (now) {
+    case "Sunday":
+      console.log("sun hello");
+      val = "Sun";
+      text = "Closed today";
+      day = "Sun Closed";
+      break;
+    case "Monday":
+      console.log("mon hello");
+      val = "Mon";
+      text = "Open today 09:00 am – 05:00 pm";
+      day = "Mon 09:00 am – 05:00 pm";
+      break;
+    case "Tuesday":
+      console.log("tue hello");
+      val = "Tue";
+      text = "Open today 09:00 am – 05:00 pm";
+      day = "Tue 09:00 am – 05:00 pm";
+      break;
+    case "Wednesday":
+      console.log("wed hello");
+      val = "Wed";
+      text = "Open today 09:00 am – 05:00 pm";
+      day = "Wed 09:00 am – 05:00 pm";
+      break;
+    case "Thursday":
+      console.log("thu hello");
+      val = "Thu";
+      text = "Open today 09:00 am – 05:00 pm";
+      day = "Thu 09:00 am – 05:00 pm";
+      break;
+    case "Friday":
+      console.log("fri hello");
+      val = "Fri";
+      text = "Open today 09:00 am – 05:00 pm";
+      day = "Fri 09:00 am – 05:00 pm";
+      break;
+    default:
+      val = "Sat";
+      text = "Closed today";
+      day = "Sat Closed";
+  }
+  const selectedText = { text };
+  const selectedDay = { day };
+  const selectedVal = { val };
+  const all = [{ selectedText, selectedDay, selectedVal }];
+  console.log(all);
+
+  return (
+    <div className="flex justify-end">
+      <div className="min-w-[1400px]">
+        <div className="flex justify-between w-full">
+          <div className="w-full">
+            <h1 className="text-5xl text-[#f76600] mb-10 font-bold playrify text-left">
+              Contact us at the
+              <br />
+              Hicksville Office
+            </h1>
+            <p className="text-2xl text-[#1b1b1b] mb-6">
+              Better yet, see us in person!
+            </p>
+            <p className="text-lg text-[#5e5e5e] mb-6 ">
+              We love our customers, so feel free to visit during normal
+              business hours.
+            </p>
+            <p className="text-2xl text-[#1b1b1b] mb-6">Cottage Home Care</p>
+            <p className="text-lg text-[#5e5e5e] mb-6 ">
+              25 Newbridge Road, Suite 302, Hicksville, New York 11801, United
+              States
+            </p>
+            <div>
+              <p className="text-2xl text-[#1b1b1b] mb-6">Hours</p>
+              <select className="focus:outline-none text-lg w-96 py-3">
+                {all.map((al, index) => (
+                  <option value={al.selectedVal.val} key={index}>
+                    {al.selectedText.text}
+                  </option>
+                ))}
+                <option value="Mon">Mon 09:00 am – 05:00 pm</option>
+                <option value="Tue">Tue 09:00 am – 05:00 pm</option>
+                <option value="Wed">Wed 09:00 am – 05:00 pm</option>
+                <option value="Thu">Thu 09:00 am – 05:00 pm</option>
+                <option value="Fri">Fri 09:00 am – 05:00 pm</option>
+                <option value="Sat">Sat Closed</option>
+                <option value="Sun">Sun Closed</option>
+              </select>
+            </div>
+            <button className="uppercase text-base bg-[#ff6900] rounded py-4 mt-6 px-8 text-black cursor-pointer font-bold">
+              Contact The Hicksville Office
+            </button>
+          </div>
+          <div>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3021.7242232894528!2d-73.52912308452832!3d40.76808987932589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c280e280b85923%3A0x6d08773ea58e3908!2s25%20Newbridge%20Rd%20Unit%20302%2C%20Oyster%20Bay%2C%20NY%2011801%2C%20USA!5e0!3m2!1sen!2sbd!4v1677688110766!5m2!1sen!2sbd"
+              width="800"
+              height="630"
+              style={{ border: "0" }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+              title="Hicksville Office"
+            ></iframe>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Contacts;
--- src/Components/Pages/Services/Stressed/Stressed.js
+++ src/Components/Pages/Services/Stressed/Stressed.js
@@ -7,21 +7,39 @@ const Stressed = () => {
   return (
     <>
       <div className="bg-[#EBF8F9]">
-      <div className="my-20  py-10" data-aos="fade-up" data-aos-duration="2000">
-        <h1 className="text-[#322e51] pb-2 text-3xl lg:text-[35px] relative font-bold font-[PlayfairDisplay] afterEffect mb-5 text-center px-5">
-        Services
-        </h1>
-        <p className="text-center tracking-wide text-lg md:text-xl text-[#67637e] px-5 font-bold">
-        A Little About What We Do
-        </p>
-        <p className="text-[#67637E] mt-8 w-[90%] md:w-[70%] mx-auto lg:text-center text-justify">
-        Our experienced and highly trained caregivers provide the highest quality of care to both patients and their families. Our goal is to help individuals and their loved ones live happy and fulfilling lives. Arthritis, physical disabilities, and age-related conditions are some of the few circumstances that prevent patients from caring for themselves. Our caregivers can assist you with daily tasks such as mobility, eating, exercising, and grooming. We even offer Post-Surgical Assistance, recovering from home after surgery can limit your daily activities. Whether you are recovering from major surgery, childbirth, or plastic surgery, our compassionate caregivers can assist you to a healthy and complete recovery.
-        </p>
-      </div>
+        <div
+          className="my-20  py-10"
+          data-aos="fade-up"
+          data-aos-duration="2000"
+        >
+          <h1 className="text-[#322e51] pb-2 text-3xl lg:text-[35px] relative font-bold font-[PlayfairDisplay] afterEffect mb-5 text-center px-5">
+            Services
+          </h1>
+          <p className="text-center tracking-wide text-lg md:text-xl text-[#67637e] px-5 font-bold">
+            A Little About What We Do
+          </p>
+          <p className="text-[#67637E] mt-8 w-[90%] md:w-[70%] mx-auto lg:text-center text-justify">
+            Our experienced and highly trained caregivers provide the highest
+            quality of care to both patients and their families. Our goal is to
+            help individuals and their loved ones live happy and fulfilling
+            lives. Arthritis, physical disabilities, and age-related conditions
+            are some of the few circumstances that prevent patients from caring
+            for themselves. Our caregivers can assist you with daily tasks such
+            as mobility, eating, exercising, and grooming. We even offer
+            Post-Surgical Assistance, recovering from home after surgery can
+            limit your daily activities. Whether you are recovering from major
+            surgery, childbirth, or plastic surgery, our compassionate
+            caregivers can assist you to a healthy and complete recovery.
+          </p>
+        </div>
       </div>
       <div className="px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-6xl md:px-24 lg:px-8">
         <div className="flex flex-col items-center justify-between lg:flex-row">
-          <div className="mb-10  lg:max-w-lg lg:pr-5 lg:mb-0" data-aos="fade-right" data-aos-duration="2000">
+          <div
+            className="mb-10  lg:max-w-lg lg:pr-5 lg:mb-0"
+            data-aos="fade-right"
+            data-aos-duration="2000"
+          >
             <div className="max-w-xl mb-6 md:px-14">
               <p className="max-w-lg  text-lg md:text-2xl font-semibold tracking-tight text-[#67637e]  ">
                 HHA / PCA
@@ -60,7 +78,11 @@ const Stressed = () => {
               </div>
             </div>
           </div>
-          <div className="relative lg:w-1/2" data-aos="fade-down" data-aos-duration="2000">
+          <div
+            className="relative lg:w-1/2"
+            data-aos="fade-down"
+            data-aos-duration="2000"
+          >
             <img
               className="object-cover w-full  rounded "
               src={image1}
@@ -72,18 +94,25 @@ const Stressed = () => {
 
       <div className="px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-6xl md:px-24 lg:px-8 lg:py-24 py-16">
         <div className="flex flex-col items-center justify-between lg:flex-row">
-          <div className="relative lg:w-1/2 mb-10 lg:mb-0" data-aos="fade-down" data-aos-duration="2000">
+          <div
+            className="relative lg:w-1/2 mb-10 lg:mb-0"
+            data-aos="fade-down"
+            data-aos-duration="2000"
+          >
             <img
               className="object-cover w-full  rounded "
               src={image2}
               alt=""
             />
           </div>
-          <div className="mb-10 lg:max-w-lg lg:pr-5 lg:mb-0" data-aos="fade-left" data-aos-duration="2000">
-         
+          <div
+            className="mb-10 lg:max-w-lg lg:pr-5 lg:mb-0"
+            data-aos="fade-left"
+            data-aos-duration="2000"
+          >
             <div className="max-w-xl mb-6 md:px-11">
-            <p className="max-w-lg  text-lg md:text-2xl font-semibold tracking-tight text-[#67637e] ">
-              CDPAP
+              <p className="max-w-lg  text-lg md:text-2xl font-semibold tracking-tight text-[#67637e] ">
+                CDPAP
               </p>
               <hr className="border-[1px] border-primary w-[60px] md:w-[80px] mb-4" />
               <div className="text-base text-gray-700 md:text-lg">
