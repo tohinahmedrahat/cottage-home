4c8bf9570cc23a77753a64e577553e7bedd02e30
Razibul-Islam <razibulislam665@gmail.com>
2023-03-09 17:09:55 +0600

    banner done

--- src/Components/Pages/HhhaCertification/HhhaCertification.js
+++ src/Components/Pages/HhhaCertification/HhhaCertification.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React from "react";
+import image1 from "../../../assets/hhaBanner.jpg";
 
 const HhhaCertification = () => {
-    return (
-        <div>
-        
-        <div>
-            Banner
-
-            welcome message
+  return (
+    <div>
+      <div className="relative">
+        <img src={image1} className="w-full" alt="" />
+        <div className="absolute bottom-0 left-0 right-0 px-4 py-2 bg-black/50  h-full flex flex-col justify-center items-center">
+          <h1 className="text-white text-6xl mb-5 font-bold text-center">
+            Welcome to HHCA
+            <br />
+            Training Institute
+          </h1>
+          <p className="text-white max-w-xl text-center mx-auto text-lg">
+            Get Certified & Launch Your Career at HHCA Training Institute -
+            We're Offering Day, Evening & Weekend Classes. Call Now ENGLISH
+            (718) 298-5010,
+          </p>
+          <button className="px-5 py-3 bg-gray-700 hover:bg-gray-800 duration-300 text-white mt-5">Learn More</button>
         </div>
+      </div>
 
-            <div>
-                class card
-            </div>
-            
-            <div>
-                
-            </div>
+      <div>class card</div>
 
-        </div>
-    );
+      <div></div>
+    </div>
+  );
 };
 
 export default HhhaCertification;
--- src/Components/Routes/Route/Route.js
+++ src/Components/Routes/Route/Route.js
@@ -1,12 +1,12 @@
 import DashBoardLayout from "../../../DashBoardLayout/DashBoardLayout";
 import AllMessages from "../../AllMessages/AllMessages";
-import About from "../../Pages/About/About";
+// import About from "../../Pages/About/About";
 import AdminRoute from "../../Pages/AdminRoute/AdminRoute";
 import Cdpaps from "../../Pages/CDPAP/Cdpaps";
 import Contacts from "../../Pages/Contacts/Contacts/Contacts";
 import Covid from "../../Pages/Covid/Covid";
 import AllUsers from "../../Pages/DashBoard/AllUsers/AllUsers";
-import DashBoard from "../../Pages/DashBoard/DashBoard";
+// import DashBoard from "../../Pages/DashBoard/DashBoard";
 import ErrorPage from "../../Pages/ErrorPage/ErrorPage";
 import HhhaCertification from "../../Pages/HhhaCertification/HhhaCertification";
 import Home from "../../Pages/Home/Home/Home";
@@ -17,77 +17,99 @@ import PrivateRoute from "../../Pages/PrivateRoute/PrivateRoute";
 import Register from "../../Pages/Register/Register";
 import Services from "../../Pages/Services/Services/Services";
 
-
 const { createBrowserRouter } = require("react-router-dom");
 
 const Route = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      errorElement: <ErrorPage></ErrorPage>,
-      children:[
-        {
-            path:"/",
-            element:<Home></Home>
-        },
-        {
-            path:"/home",
-            element:<Home></Home>
-        },
-        {
-            path:"/services",
-            element:<Services></Services>
-        },
-        {
-            path:"/contacts",
-            element:<PrivateRoute><Contacts></Contacts></PrivateRoute>
-        },
-        {
-            path:"/covid",
-            element:<Covid></Covid>
-        },
-        {
-            path:"/login",
-            element:<Login></Login>
-        },
-        {
-            path:"/register",
-            element:<Register></Register>
-        },
-        {
-            path:"/register",
-            element:<Register></Register>
-        },
-        {
-            path:"/hhaCertification",
-            element:<HhhaCertification></HhhaCertification>
-        },
-        
-       
-      ]
-    },
-    {
-        path:'/dashboard',
-        element:<PrivateRoute> <DashBoardLayout></DashBoardLayout></PrivateRoute>,
-        errorElement:<ErrorPage></ErrorPage>,
-        children:[
-            {
-                path:'/dashboard',
-                element:<AdminRoute><AllMessages></AllMessages> </AdminRoute>
-            },
-            {
-                path:'/dashboard/cdpap',
-                element:<AdminRoute><Cdpaps></Cdpaps> </AdminRoute>
-            },
-            {
-                path:'/dashboard/pca',
-                element:<AdminRoute><Pca></Pca> </AdminRoute>
-            },
-            {
-                path:'/dashboard/users',
-                element:<AdminRoute><AllUsers> </AllUsers> </AdminRoute>
-            },
-        ]
-    }
-  ]);
-  export default Route;
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/home",
+        element: <Home></Home>,
+      },
+      {
+        path: "/services",
+        element: <Services></Services>,
+      },
+      {
+        path: "/contacts",
+        element: (
+          <PrivateRoute>
+            <Contacts></Contacts>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/covid",
+        element: <Covid></Covid>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+      {
+        path: "/hhaCertification",
+        element: <HhhaCertification></HhhaCertification>,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <PrivateRoute>
+        {" "}
+        <DashBoardLayout></DashBoardLayout>
+      </PrivateRoute>
+    ),
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/dashboard",
+        element: (
+          <AdminRoute>
+            <AllMessages></AllMessages>{" "}
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "/dashboard/cdpap",
+        element: (
+          <AdminRoute>
+            <Cdpaps></Cdpaps>{" "}
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "/dashboard/pca",
+        element: (
+          <AdminRoute>
+            <Pca></Pca>{" "}
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "/dashboard/users",
+        element: (
+          <AdminRoute>
+            <AllUsers> </AllUsers>{" "}
+          </AdminRoute>
+        ),
+      },
+    ],
+  },
+]);
+export default Route;
