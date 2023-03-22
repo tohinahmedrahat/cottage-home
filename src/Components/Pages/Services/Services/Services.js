9fa1fa4a465dba4bfde83d886bf00939024eea27
“RakibHasanDev” <rakibkst16@gmail.com>
2023-03-23 02:59:47 +0600

    Added las from rakib

--- src/Components/Pages/Services/Services/Services.js
+++ src/Components/Pages/Services/Services/Services.js
@@ -3,21 +3,26 @@ import useTitle from "../../../Hooks/useTitle";
 import Care from "../../Home/Care/Care";
 import ServiceBanner from "../Banner/ServiceBanner";
 import Stressed from "../Stressed/Stressed";
+import AboutWeDo from "./AboutWeDo";
 import Carousel from "./Carousel";
 import ServiceCard from "./ServiceCard";
 
 const Services = () => {
+  document.documentElement.style.scrollBehavior = "smooth";
   useTitle('Services');
   window.scrollTo(0, 0);
   return (
-    <div className="">
-      <div className="-z-50">
+    <div>
+      <div>
       <ServiceBanner></ServiceBanner>
       </div>
-      <div className="-mt-10 relative ">
+      
+      
       <ServiceCard></ServiceCard>
-      </div>
+      
       <Stressed></Stressed>
+      
+      
       <div className="bg-[#EBF8F9] py-20">
         <h1 className="text-[#322e51] pb-2 text-3xl lg:text-[35px] relative font-bold font-[PlayfairDisplay] afterEffect mb-10 text-center px-5">"The Right Home Care for You"</h1>
       <div className="w-[95%] lg:w-[75%] mx-auto">
--- src/Components/Pages/Services/Stressed/Stressed.js
+++ src/Components/Pages/Services/Stressed/Stressed.js
@@ -6,34 +6,8 @@ import image2 from "../../../../assets/Alzhiemers-activities2.jpg";
 const Stressed = () => {
   return (
     <>
-      <div className="bg-[#EBF8F9]">
-        <div
-          className="my-20  py-10"
-          data-aos="fade-up"
-          data-aos-duration="2000"
-        >
-          <h1 className="text-[#322e51] pb-2 text-3xl lg:text-[35px] relative font-bold font-[PlayfairDisplay] afterEffect mb-5 text-center px-5">
-            Services
-          </h1>
-          <p className="text-center tracking-wide text-lg md:text-xl text-[#67637e] px-5 font-bold">
-            A Little About What We Do
-          </p>
-          <p className="text-[#67637E] mt-8 w-[90%] md:w-[70%] mx-auto lg:text-center text-justify">
-            Our experienced and highly trained caregivers provide the highest
-            quality of care to both patients and their families. Our goal is to
-            help individuals and their loved ones live happy and fulfilling
-            lives. Arthritis, physical disabilities, and age-related conditions
-            are some of the few circumstances that prevent patients from caring
-            for themselves. Our caregivers can assist you with daily tasks such
-            as mobility, eating, exercising, and grooming. We even offer
-            Post-Surgical Assistance, recovering from home after surgery can
-            limit your daily activities. Whether you are recovering from major
-            surgery, childbirth, or plastic surgery, our compassionate
-            caregivers can assist you to a healthy and complete recovery.
-          </p>
-        </div>
-      </div>
-      <div className="px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-6xl md:px-24 lg:px-8">
+      
+      <div className="px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-6xl md:px-24 lg:px-8 mt-14" id="hha">
         <div className="flex flex-col-reverse items-center justify-between lg:flex-row">
           <div
             className="mb-10  lg:max-w-lg lg:pr-5 lg:mb-0"
@@ -46,34 +20,8 @@ const Stressed = () => {
               </p>
               <hr className="border-[1px] border-primary w-[100px] md:w-[120px] mb-4" />
               <div className="text-base text-gray-700 md:text-lg">
-                <h5 className="text-[#322e51] font-semibold mb-4">
-                  Preferred aides will provide:
-                </h5>
-                <p className="flex justify-center items-center mb-5">
-                  <span className="text-[#00a6b2] bg-[#ebf8f9] rounded-full mr-3">
-                    <HiStar className="text-3xl" />
-                  </span>
-                  <span className="text-[#2f3450] text-[1em] md:tracking-wide">
-                    Reliable medication reminders, so a dose is never forgotten.
-                  </span>
-                </p>
-                <p className="flex justify-center items-center mb-5">
-                  <span className="text-[#00a6b2] bg-[#ebf8f9] rounded-full mr-3">
-                    <HiStar className="text-3xl" />
-                  </span>
-                  <span className="text-[#2f3450] text-[1em] md:tracking-wide">
-                    Healthy, delicious meals, so your loved one gets the
-                    nutrition they need.
-                  </span>
-                </p>
-                <p className="flex justify-center items-center mb-5">
-                  <span className="text-[#00a6b2] bg-[#ebf8f9] rounded-full mr-3">
-                    <HiStar className="text-3xl" />
-                  </span>
-                  <span className="text-[#2f3450] text-[1em] md:tracking-wide">
-                    Patient daily care with tasks like bathing, dressing and
-                    grooming.
-                  </span>
+                <p className="text-justify">
+                This service will allow us to send a professionally trained and certified home health aide to take care of family or loved ones, leaving you worry-free and with peace of mind. 
                 </p>
               </div>
             </div>
@@ -92,7 +40,7 @@ const Stressed = () => {
         </div>
       </div>
 
-      <div className="px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-6xl md:px-24 lg:px-8 lg:py-24 py-16">
+      <div className="px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-6xl md:px-24 lg:px-8 lg:py-24 py-16" id="cdpap">
         <div className="flex flex-col items-center justify-between lg:flex-row">
           <div
             className="relative lg:w-1/2 mb-10 lg:mb-0"
@@ -116,40 +64,9 @@ const Stressed = () => {
               </p>
               <hr className="border-[1px] border-primary w-[60px] md:w-[80px] mb-4" />
               <div className="text-base text-gray-700 md:text-lg">
-                <p className="flex justify-center items-center mb-5">
-                  <span className="text-[#00a6b2] bg-[#ebf8f9] rounded-full mr-3">
-                    <HiStar className="text-3xl" />
-                  </span>
-                  <span className="text-[#2f3450] text-[1em] md:tracking-wide">
-                    Reminders + escorts, so appointments are never missed you.
-                  </span>
-                </p>
-                <p className="flex justify-center items-center mb-5">
-                  <span className="text-[#00a6b2] bg-[#ebf8f9] rounded-full mr-3">
-                    <HiStar className="text-3xl" />
-                  </span>
-                  <span className="text-[#2f3450] text-[1em] md:tracking-wide">
-                    Warm companionship, so your loved one is never lonely - even
-                    on the most confusion-filled days
-                  </span>
-                </p>
-                <p className="flex justify-center items-center mb-5">
-                  <span className="text-[#00a6b2] bg-[#ebf8f9] rounded-full mr-3">
-                    <HiStar className="text-3xl" />
-                  </span>
-                  <span className="text-[#2f3450] text-[1em] md:tracking-wide">
-                    A calming, steady presence that helps prevent power
-                    struggles and flare-ups.
-                  </span>
-                </p>
-                <p className="flex justify-center items-center mb-5">
-                  <span className="text-[#00a6b2] bg-[#ebf8f9] rounded-full mr-3">
-                    <HiStar className="text-3xl" />
-                  </span>
-                  <span className="text-[#2f3450] text-[1em] md:tracking-wide">
-                    Unparalleled protection from frightening, dangerous
-                    wandering.
-                  </span>
+                
+                <p className="text-justify">
+                This service allows us to directly pay you or a friend to take care of your own loved ones, allowing them to be reimbursed for their hard work!
                 </p>
               </div>
             </div>
