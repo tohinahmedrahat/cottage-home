6061d1978ef6e8d85ca73ffc2da45c1b5cb4408f
“RakibHasanDev” <rakibkst16@gmail.com>
2023-03-31 20:44:13 +0600

    added from rakib hasan

--- src/Components/FooterComponents/MissionVision.js
+++ src/Components/FooterComponents/MissionVision.js
@@ -1,34 +1,72 @@
 import React from 'react';
+import pic1 from '../../../src/assets/mission-vision/mission-vision.jpg'
+import pic3 from '../../../src/assets/mission-vision/mission-vission-2.jpg'
+import pic2 from '../../../src/assets/mission-vision/vission2.jpeg'
+import pic4 from '../../../src/assets/mission-vision/vission3.jpeg'
 
 const MissionVision = () => {
-    return (
-        <div>
+  window.scrollTo(0, 0);
+  return (
+    <div className='min-h-screen'>
+
+      <div className='md:w-[85%] w-[95%] mx-auto mt-5 mb-20'>
+        <h1 className='text-center my-5 playrify font-semibold text-primary md:text-3xl text-2xl'> Mission And Vision Of Cottage Care </h1>
+        <div className='md:grid grid-cols-8  gap-8 items-center'>
+          <div className='md:col-span-4 lg:col-span-2 text-gray-600 mt-5'>
+            <p className='text-justify'>
+              Our mission is to provide personalized, compassionate, and high-quality home care services that enable our patients to maintain their independence and improve their overall health and well-being.
+            </p>
+
+
+            <p className='text-justify mt-3'>
+              We strive to deliver individualized care plans tailored to each patient's unique needs and preferences. We are also committed to establishing lasting bonds of respect, confidence, and open communication with our patients and their families.
+            </p>
+
+          </div>
+          <div className='lg:col-span-2 md:col-span-4  mt-5 '>
+
+            <img src={pic1} alt="" className='w-full md:h-96' />
+
+          </div>
+          <div className='col-span-4 mt-5 hidden lg:block'>
+            <img src={pic2} alt="" className='w-full md:h-96' />
+
+          </div>
+
+        </div>
+
+        <div className='md:grid grid-cols-8  gap-8 items-center mt-3'>
+          <div className='col-span-4 mt-5 hidden lg:block'>
+            <img src={pic4} alt="" className='w-full md:h-96' />
+
+          </div>
+          <div className='lg:col-span-2 md:col-span-4  mt-5 '>
+
+            <img src={pic3} alt="" className='w-full md:h-96' />
+
+          </div>
+          <div className='md:col-span-4 lg:col-span-2 text-gray-600 mt-5'>
+            <p className='text-justify'>
+            Our team of highly qualified and dedicated caregivers is passionate about improving the lives of those we serve. Cottage Home Care Services is committed to delivering exceptional care with dignity, integrity, and professionalism.
+            </p>
+
+
             
-            <header
-  class="relative flex items-center justify-center h-screen mb-12 overflow-hidden"
->
-  <div
-    class="relative  p-5 text-2xl text-white bg-purple-300 bg-opacity-50 rounded-xl"
-    style={{zIndex:"1"}}
-  >
-    Welcome to my site!
-  </div>
-  <video
-    autoplay="{true}"
-    loop
-    muted
-    class="absolute  w-[100%]"
-  >
-    <source
-      src="https://assets.mixkit.co/videos/preview/mixkit-set-of-plateaus-seen-from-the-heights-in-a-sunset-26070-large.mp4"
-      type="video/mp4"
-    />
-  </video>
-</header>
+
+          </div>
+
 
 
         </div>
-    );
+
+      </div>
+
+
+
+
+
+    </div>
+  );
 };
 
 export default MissionVision;
