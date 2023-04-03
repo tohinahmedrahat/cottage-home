4bc41dc5079fb3d772460b87655297eac6f1cfd5
“RakibHasanDev” <rakibkst16@gmail.com>
2023-04-03 20:11:30 +0600

    added from rakib

--- src/Components/FooterComponents/TeamMembers.js
+++ src/Components/FooterComponents/TeamMembers.js
@@ -233,7 +233,8 @@ const TeamMembers = () => {
                 </div>
                 <div className="w-full bg-gray-100 px-10 pt-10">
                     <div className="container mx-auto">
-                        <div className="lg:flex md:flex sm:flex items-center xl:justify-between flex-wrap md:justify-around sm:justify-around lg:justify-around">
+                        <div className="lg:flex md:flex sm:flex items-center xl:justify-between flex-wrap md:justify-around sm:justify-around lg:justify-around team-member">
+                        {/* lg:flex md:flex sm:flex items-center xl:justify-between flex-wrap md:justify-around sm:justify-around lg:justify-around */}
                             {
                                 employers?.map((employer,index)=><div 
                                 
@@ -242,6 +243,8 @@ const TeamMembers = () => {
                                 data-aos="zoom-in"
                                 data-aos-duration="2000"
                                 >
+
+{/* xl:w-1/3 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5 */}
 
 
                                 <div className="rounded overflow-hidden shadow-md bg-white"
--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC73n9GKVxsaj2XSheC5J9XjwxYziTmLcE",
-  authDomain: "cottage-home-care-4f1a5.firebaseapp.com",
-  projectId: "cottage-home-care-4f1a5",
-  storageBucket: "cottage-home-care-4f1a5.appspot.com",
-  messagingSenderId: "1038075022406",
-  appId: "1:1038075022406:web:50e2a45da0b3919e8f8e47"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
