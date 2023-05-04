29ecfc74b47e034b23a8d3511108b359c13c3c35
“RakibHasanDev” <rakibkst16@gmail.com>
2023-05-05 01:48:37 +0600

    added video media query

--- src/Components/Pages/Home/Banner/Banner.js
+++ src/Components/Pages/Home/Banner/Banner.js
@@ -13,14 +13,14 @@ const Banner = () => {
   return (
     <div>
       <NewVideoBanner></NewVideoBanner>
-      {/* <div
-        className="  bg-no-repeat bg-cover bg-center text-center banner"
-        
+      <div
+        className="  bg-no-repeat bg-cover bg-center text-center banner block md:hidden"
+
       >
         <div className=" tracking-[1px] mx-auto pt-40">
           <div className="h-[150px] max-w-2xl mx-auto main-text mt-[10vh]">
             <h1 className="md:text-6xl text-4xl px-[15px] text-white font-[PlayfairDisplay] ">
-              
+
               RIGHT HOME CARE FOR YOU
             </h1>
           </div>
@@ -36,7 +36,7 @@ const Banner = () => {
                   </p>
                   <p className="text-white text-xl text-justify">-</p>
                   <p className="text-white text-sm text-justify font-normal">
-                  Our company focuses on enhancing and building our people - staff, patients, and all others; a principle that has earned us a reputation for being generous, so much so that Cottage Care is known for lending a helping hand to seniors throughout New York City. 
+                    Our company focuses on enhancing and building our people - staff, patients, and all others; a principle that has earned us a reputation for being generous, so much so that Cottage Care is known for lending a helping hand to seniors throughout New York City.
                   </p>
                   <h4 className=" md:text-xl text-sm font-semibold text-[#dbf2f4] font-[Roboto] mb-8 px-5 md:px-0 mt-5">
                     “HHA/PCA & CDPAP SERVICES THROUGHOUT NEW YORK CITY, NASSAU & SUFFOLK COUNTY, WESTCHESTER & ALBANY”
@@ -54,7 +54,7 @@ const Banner = () => {
             </div>
           </div>
         </div>
-      </div> */}
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-3">
         <div className="flex justify-center bg-[#DBF2F4] py-6">
           <div className=" flex items-center gap-3 lg:gap-5   ">
